b31dc9ed238104877cbcd07a326152eb03284c87
160209-wyj <68934822+160209-wyj@users.noreply.github.com>
2020-08-10 10:04:01 +0800

    Add files via upload
    视频截帧

---
+++ video.py
@@ -0,0 +1,62 @@
+#-------------------------------------#
+#       调用摄像头检测
+#-------------------------------------#
+from PIL import Image
+import numpy as np
+import cv2
+import time
+import os
+import tqdm
+#视频文件夹
+video = r'./公章/'
+video_path = os.listdir(video)
+# 调用摄像头
+c = 1
+for vid in tqdm.tqdm(video_path):
+    
+    capture=cv2.VideoCapture(video+vid) # capture=cv2.VideoCapture("1.mp4")
+    
+    timeRate = 0.5  # 截取视频帧的时间间隔（这里是每隔10秒截取一帧）
+    fps = 0.0
+    while(True):
+        t1 = time.time()
+        # 读取某一帧
+        ref,frame=capture.read()
+        # 这个是获取视频帧率
+        FPS = capture.get(5)
+        if ref:
+            frameRate = int(FPS) * timeRate
+            if(c % frameRate == 0):
+                # 读取某一帧
+                ref,frame=capture.read()
+                # frame = cv2.imread('000600.jpg')
+                # 格式转变，BGRtoRGB
+                try:
+                    frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+                except:
+                    print('视频名称：',vid)
+                    continue
+                # 转变成Image
+                frame = Image.fromarray(np.uint8(frame))
+
+                # 进行检测
+                # frame = np.array(yolo.detect_image(frame))
+
+                # RGBtoBGR满足opencv显示格式
+                
+                frame = cv2.cvtColor(np.float32(frame),cv2.COLOR_RGB2BGR)
+                
+                fps  = ( fps + (1./(time.time()-t1)) ) / 2
+                # print("fps= %.2f"%(fps))
+                # frame = cv2.putText(frame, "fps= %.2f"%(fps), (0, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                cv2.imwrite(r'./gz_p/{}.jpg'.format(c),frame)
+            c+=1
+        else:
+            break
+            # cv2.imshow("video",frame)
+
+    capture.release()
+    # c= cv2.waitKey(30) & 0xff 
+    # if c==27:
+    #     capture.release()
+    #     break
